c1085dadb1c0de96d2bf6a9988761d0f4295d930
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-03-27 22:16:49 +0700

    menambahkan script bootstrap css&js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <title>Salon SisterBeautyBar</title>
 </head>
 <body>
 
+
+
+    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
 </html>
